d6b8a5bf9e8c523248f57c77299e20246fadc0fb
tuomonen <heli.tuomonen@cs.helsinki.fi>
2017-05-04 13:23:57 +0300

    fixies

--- app/src/main/java/dis/bluetooth21/MainActivity.java
+++ app/src/main/java/dis/bluetooth21/MainActivity.java
@@ -1,5 +1,6 @@
 package dis.bluetooth21;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -12,8 +13,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,33 +25,45 @@ import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.os.Vibrator;
+import android.os.SystemClock;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView heartRateNumber;
+    TextView timer;
+    ListView lv;
+    private Handler pHandler = new Handler();
+    int num;
+    boolean x;
+    boolean isStart;
+    CoordinatorLayout cL;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+    int Seconds, Minutes, MilliSeconds, Hours ;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeScanner mBluetoothLeScanner;
-
     private boolean mScanning;
-
     private static final int RQS_ENABLE_BLUETOOTH = 1;
-
     Button btnScan;
     ListView listViewLE;
-
     List<BluetoothDevice> listBluetoothDevice;
     ListAdapter adapterLeScanResult;
-
     private Handler mHandler;
     private static final long SCAN_PERIOD = 10000;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        isStart=false;
         // Check if BLE is supported on the device.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this,
@@ -59,6 +74,25 @@ public class MainActivity extends AppCompatActivity {
 
         getBluetoothAdapterAndLeScanner();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check 
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+
+
+
         // Checks if Bluetooth is supported on the device.
         if (mBluetoothAdapter == null) {
             Toast.makeText(this,
@@ -67,6 +101,16 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timestart();
+
+            }
+        });
+        */
 
         btnScan = (Button)findViewById(R.id.scan);
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -208,16 +252,22 @@ public class MainActivity extends AppCompatActivity {
             listViewLE.invalidateViews();
 
             // Stops scanning after a pre-defined scan period.
+
             mHandler.postDelayed(new Runnable() {
                 @Override
+
                 public void run() {
                     mBluetoothLeScanner.stopScan(scanCallback);
                     listViewLE.invalidateViews();
-
-                    Toast.makeText(MainActivity.this,
-                            "Scan timeout",
-                            Toast.LENGTH_LONG).show();
-
+                    if(!listBluetoothDevice.isEmpty()) {
+                        Toast.makeText(MainActivity.this,
+                                "Scan timeout",
+                                Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(MainActivity.this,
+                                "Did not find anything",
+                                Toast.LENGTH_LONG).show();
+                    }
                     mScanning = false;
                     btnScan.setEnabled(true);
                 }
@@ -231,7 +281,7 @@ public class MainActivity extends AppCompatActivity {
             mScanning = false;
             btnScan.setEnabled(true);
         }
-    }
+    };
 
     private ScanCallback scanCallback = new ScanCallback() {
         @Override
@@ -264,4 +314,106 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    /*
+    public Runnable stopwatch = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Hours = Minutes / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+            timer = (TextView) findViewById(R.id.timer);
+            timer.setText("" + Hours + ":" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%02d", MilliSeconds));
+            if (isStart == true) {
+                pHandler.postDelayed(this, 0);
+            }
+        }
+    };
+    private Runnable updateTask = new Runnable () {
+        public void run() {
+            Log.d(getString(R.string.app_name) + " ChatList.updateTask()",
+                    "updateTask run!");
+            if(x==true) {
+                num = num + 1;
+                if(num>108){
+                    x=false;
+                }
+            }else{
+                num=num-1;
+            }
+            heartRateNumber = (TextView) findViewById(R.id.hrNum);
+            heartRateNumber.setText(""+num);
+            cL=(CoordinatorLayout) findViewById(R.id.bg);
+            if(num>100){
+                cL.setBackgroundColor(0xffff0000);
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(400);
+            }else if(num>80 && num<101){
+                cL.setBackgroundColor(0xffffff00);
+            }else{
+                cL.setBackgroundColor(0xffffffff);
+            }
+
+            if(isStart==true){
+                pHandler.postDelayed(updateTask,500);
+
+            }else{
+                heartRateNumber.setText("Stop");
+                cL.setBackgroundColor(0xffffffff);
+            }
+        }
+    };
+    private void timestart(){
+        if(isStart==false){
+            isStart=true;
+            num=70;
+            heartRateNumber = (TextView) findViewById(R.id.hrNum);
+            timer = (TextView) findViewById(R.id.timer);
+            timer.setText("00:00:00:00");
+            heartRateNumber.setText(""+num);
+            StartTime = SystemClock.uptimeMillis();
+            pHandler.postDelayed(stopwatch, 0);
+            pHandler.postDelayed(updateTask, 500);
+            x=true;
+        }else{
+            isStart=false;
+            pHandler.postDelayed(updateTask, 0);
+        }
+    };
+    */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
 }
